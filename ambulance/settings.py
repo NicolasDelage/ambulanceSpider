b26edca7d9fcd8a9eefbf331df3840975d601af2
NicolasDelage <nicolasdelage7@gmail.com>
2022-04-14 09:34:16 +0200

    feat: evol

--- ambulance/settings.py
+++ ambulance/settings.py
@@ -13,75 +13,86 @@ SPIDER_MODULES = ['ambulance.spiders']
 NEWSPIDER_MODULE = 'ambulance.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'ambulance (+http://www.yourdomain.com)'
+# USER_AGENT = 'ambulance (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
 
+# ROTATING_PROXY_LIST = [
+#     '51.195.76.214',
+#     '176.31.129.223',
+#     '62.210.119.138',
+#     '165.225.76.150',
+#     '188.165.59.127',
+#     '62.75.229.155',
+# ]
+
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+# CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+# DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+# CONCURRENT_REQUESTS_PER_DOMAIN = 16
+# CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+# COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+# TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+# DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
-#}
+# }
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
+# SPIDER_MIDDLEWARES = {
 #    'ambulance.middlewares.AmbulanceSpiderMiddleware': 543,
-#}
+# }
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'ambulance.middlewares.AmbulanceDownloaderMiddleware': 543,
-#}
+# DOWNLOADER_MIDDLEWARES = {
+    # 'ambulance.middlewares.AmbulanceDownloaderMiddleware': 543,
+    # 'rotating_proxies.middlewares.RotatingProxyMiddleware': 610,
+    # 'rotating_proxies.middlewares.BanDetectionMiddleware': 620,
+# }
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
+# }
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
+# ITEM_PIPELINES = {
 #    'ambulance.pipelines.AmbulancePipeline': 300,
-#}
+# }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+# AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+# AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+# AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+# AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = 'httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+# HTTPCACHE_ENABLED = True
+# HTTPCACHE_EXPIRATION_SECS = 0
+# HTTPCACHE_DIR = 'httpcache'
+# HTTPCACHE_IGNORE_HTTP_CODES = []
+# HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
--- ambulance/spiders/ambulance.py
+++ ambulance/spiders/ambulance.py
@@ -15,16 +15,13 @@ class AmbulanceCompanyNamesSpider(scrapy.Spider):
             writer.writerow(header)
 
     def start_requests(self):
-        urls = [
-            'https://www.infogreffe.fr/entreprises-francaises-par-departement.html',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+        url = 'https://www.infogreffe.fr/entreprises-francaises-par-departement.html'
+        yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         urls = response.xpath('//div[@class="listeEntreprise"]/div/h3/div/a/@href').getall()
 
-        for url in urls:
+        for url in urls[0:1]:
             first_url_part = url.split('/')[1]
 
             second_url_part = url.split('/')[-1]
@@ -47,9 +44,10 @@ class AmbulanceCompanyNamesSpider(scrapy.Spider):
 
             yield scrapy.Request(url='http://www.google.com/search?q=' + company_name.replace(' ', '+'),
                                  callback=self.parse_website_url,
-                                 meta={'company_name': 'name'})
+                                 meta={'company_name': company_name})
 
     def parse_website_url(self, response):
+        print('PARSE WEBSITE URL')
         soup = BeautifulSoup(response.body, 'html.parser')
         div = soup.find('div', text='Site Web')
         if div:
@@ -58,20 +56,20 @@ class AmbulanceCompanyNamesSpider(scrapy.Spider):
 
             yield scrapy.Request(url='http://www.google.com/search?q=%40' + website_name + '+contact',
                                  callback=self.parse_email,
-                                 meta={'company_name': 'name', 'website_url': 'https://www.' + website_name})
+                                 meta={'company_name': response.meta['company_name'], 'website_url': 'https://www.' + website_name})
 
     def parse_email(self, response):
+        print('PARSE EMAIL')
         soup = BeautifulSoup(response.body, 'html.parser')
+        match_email = None
+        match_phone = None
+        for text in soup.findAll(text=True):
+            if match_email is None:
+                match_email = re.search(r'[\w.+-]+@[\w-]+\.[\w.-]+', text)
+            if match_phone is None:
+                match_phone = re.search(r'[\+\(]?[0-9][0-9 .\-\(\)]{8,}[0-9]', text)
 
-        for item in soup.find_all("div"):
-            if 'Contact:' in item.text:
-                match = re.search(r'[\w.+-]+@[\w-]+\.[\w.-]+', item.text)
-                match_phone = re.search(r'[\+\(]?[0-9][0-9 .\-\(\)]{8,}[0-9]', item.text)
-
-                if match and match.group(0):
-                    print([response.meta['company_name'], response.meta['website_url'],
-                                         match.group(0), match_phone.group(0)])
-                    with open(self.filename, 'a+') as f:
-                        writer = csv.writer(f, delimiter=';')
-                        writer.writerow([response.meta['company_name'], response.meta['website_url'],
-                                         match.group(0), match_phone.group(0)])
+        with open(self.filename, 'a+') as f:
+            writer = csv.writer(f, delimiter=';')
+            writer.writerow([response.meta['company_name'], response.meta['website_url'],
+                             match_email.group(0) if match_email else '', match_phone.group(0) if match_phone else ''])
